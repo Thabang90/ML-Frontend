0979d30c3483b86496980b267a0765d9fdb524d0
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2023-10-20 23:20:05 +0200

    list

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ console.log(__dirname + '/dist')
 app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/index.html')); });
 app.listen(process.env.PORT || 8080);
 
+const fs = require('fs');
+
+const directoryPath = __dirname + '/dist'; // Replace with the path to your directory
+
+fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+    } else {
+        console.log('Files in the directory:');
+        files.forEach(file => {
+            console.log(file);
+        });
+    }
+});
 
 
 
