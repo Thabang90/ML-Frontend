2578cf438bd99c3bc50cceb5bdb94f82c184dcbc
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2023-10-20 23:14:18 +0200

    test

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
+console.log(__dirname + '/dist')
 app.get('/*', function(req, res) { res.sendFile(path.join(__dirname + '/dist/index.html')); });
 app.listen(process.env.PORT || 8080);
+
+
 
 
 /* const express = require('express');
