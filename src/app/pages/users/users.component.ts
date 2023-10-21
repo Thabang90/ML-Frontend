ba4ef4dc0330e3e1d9159f091b225add5615f48a
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2023-10-21 02:04:49 +0200

    stage 4

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,63 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { UserService } from 'app/shared/services/user.service';
-import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { ToastrService } from 'ngx-toastr';
-import { finalize } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UserService } from "app/shared/services/user.service";
+import { BlockUI, NgBlockUI } from "ng-block-ui";
+import { ToastrService } from "ngx-toastr";
+import { finalize } from "rxjs";
 
 @Component({
-    selector: 'userscmp',
-    templateUrl: 'users.component.html'
+  selector: "userscmp",
+  templateUrl: "users.component.html",
 })
+export class UsersComponent implements OnInit {
+  @BlockUI() blockUI: NgBlockUI;
+  users = [
+    { username: "user1", email: "user1@example.com" },
+    { username: "user2", email: "user2@example.com" },
+    { username: "user3", email: "user3@example.com" },
+    { username: "user4", email: "user4@example.com" },
+    { username: "user5", email: "user5@example.com" },
+  ];
+  userForm: FormGroup;
 
-export class UsersComponent implements OnInit{
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService,
+    private fb: FormBuilder
+  ) {}
 
-    @BlockUI() blockUI: NgBlockUI;
-    users = [
-        { username: "user1", email: "user1@example.com" },
-        { username: "user2", email: "user2@example.com" },
-        { username: "user3", email: "user3@example.com" },
-        { username: "user4", email: "user4@example.com" },
-        { username: "user5", email: "user5@example.com" }
-      ];
-    userForm: FormGroup;
+  ngOnInit() {
+    this.userForm = this.fb.group({
+      username: ["", Validators.required],
+      email: ["", Validators.required],
+    });
 
-    constructor(
-        private userService: UserService,
-        private toastr: ToastrService,
-        private fb: FormBuilder
-    ) {}
+    //this.getUsers();
+  }
 
-    ngOnInit(){
-        this.userForm = this.fb.group({
-            username: ['', Validators.required],
-            email: ['', Validators.required]
-        })
-        
-        //this.getUsers();
-    }
+  getUsers() {
+    this.blockUI.start("Retrieving a list of users...");
+    this.userService
+      .getUsers()
+      .pipe(finalize(() => this.blockUI.stop()))
+      .subscribe({
+        next: (users) => {
+          this.users = users;
+        },
+        error: (err) => {
+          this.toastr.error(err.message);
+        },
+      });
+  }
 
-    getUsers() {
-        this.blockUI.start('Retrieving a list of users...');
-        this.userService.getUsers()
-        .pipe(finalize(() => this.blockUI.stop()))
-        .subscribe({
-          next: (users) => {
-            this.users = users;
-          },
-          error: (err) => {
-            this.toastr.error(err.message);
-          }
-        })
-    }
+  editUser(user: any) {
+    this.userForm.patchValue(user);
+  }
 
-    editUser(user: any){
-        this.userForm.patchValue(user);
-    }
-
-    updateUser() {
-        this.blockUI.start('Updating user...');
-        if(this.userForm.valid) {
+  updateUser() {
+    this.blockUI.start("Updating user...");
+    /* if(this.userForm.valid) {
             this.userService.upsertUser(this.userForm.value)
             .pipe(finalize(() => this.blockUI.stop()))
             .subscribe({
@@ -72,21 +71,27 @@ export class UsersComponent implements OnInit{
                     this.toastr.error(err.message);
                 }
             })
-        }
-    }
+        } */
+    var buttonRef = document.getElementById("closeBtn");
+    buttonRef?.click();
+    this.toastr.success("User has been updated!");
+  }
 
-    deleteUser(userId: number) {
-        this.blockUI.start('Deleting user...');
-        this.userService.deleteUser(userId)
-        .pipe(finalize(() => this.blockUI.stop()))
-        .subscribe({
-            next:(res) => {
-                this.toastr.success(res.message);
-                this.getUsers();
-            },
-            error:(err) => {
-                this.toastr.error(err.message);
-            }
-        })
-    }
+  deleteUser(userId: number) {
+    this.blockUI.start("Deleting user...");
+    /* this.userService
+      .deleteUser(userId)
+      .pipe(finalize(() => this.blockUI.stop()))
+      .subscribe({
+        next: (res) => {
+          this.toastr.success(res.message);
+          this.getUsers();
+        },
+        error: (err) => {
+          this.toastr.error(err.message);
+        },
+      });
+  } */
+  this.blockUI.stop();
+}
 }
