c0b58c86b917d134c1089d3814b85429dacea1fa
unknown <thabangsepeng90@gmail.com>
2023-11-25 08:26:00 +0200

    sd

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  private baseUrl: string ="/api/user/";
+  private baseUrl: string ="http://thabang90-001-site1.atempurl.com/api/User/";
   constructor(private http: HttpClient) { }
 
   
